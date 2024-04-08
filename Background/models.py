9f55386cb548b924b5a231fc29e769407dd8b945
Favour Abilogun-Wole Oluwasayo <favyabils125@gmail.com>
2024-04-08 11:12:36 +0100

    More changes

--- Background/models.py
+++ Background/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Person(models.Model): #"Person" is a table
+#"name" is a column
+    name =  models.CharField(max_length=150) #"models."fieldname"()Tells the type of datatype that can be stored in the column "name
+    age = models.IntegerField()
+    occupation = models.CharField(max_length= 200)
